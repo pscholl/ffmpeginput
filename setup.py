9ac76a36443c67c90b261650d7c8df7d23320ba5
Philipp M. Scholl <pscholl@ese.uni-freiburg.de>
2017-02-21 20:32:03 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name = 'ffmpipe',
+       version = '1.0',
+       description = 'wrapper for ffmpeg/ffprobe for unix-like pipe I/O',
+
+       author = 'Philipp M. Scholl',
+       author_email = 'pscholl@ese.uni-freiburg.de',
+
+       py_modules = ['ffmpipe']
+      )
